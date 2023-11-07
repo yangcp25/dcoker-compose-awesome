d9d030deb3ce43ffc9e77c884dad4e60be03b560
ycp <820363773@qq.com>
2023-11-07 20:20:57 +0800

    [feat] init

--- dev/deploy.sh
+++ dev/deploy.sh
@@ -1,3 +1,4 @@
+docker network create mynet
 docker-compose -f ./sql-docker-compose.yml up -d
 docker-compose -f ./sql-docker-compose.yml up -d nginx
 docker-compose -f ./sql-docker-compose.yml restart nginx
